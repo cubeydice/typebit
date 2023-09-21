d30a591798f67610b7f1f0ee405706fdb7fe44f3
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-09-21 10:58:33 -0700

    Player animation fix

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -60,8 +60,8 @@ export default class Player {
   }
 
   draw(ctx) {
-    if (this.outOfHealth() && this.position >= 5){
-    } else if (this.hurts && this.position >= 5) {
+    if (this.outOfHealth() && this.position >= 4){
+    } else if (this.hurts && this.position >= 4) {
       this.hurts = false;
       !this.running ? this.walk() : this.run()
     } else {
